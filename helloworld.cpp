8df2cbda1f76d6d839feceb75d187bd146850bfb
nextdreamblue <zxw520blue1@163.com>
2016-10-19 11:20:52 +0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -8,6 +8,6 @@ by nextdreamblue
 
 int main()
 {
-  std::cout<<"hello world! I am coming and I will not go before my name be known by everyone in the world"<<std::endl;
+  std::cout<<"hello world! I am coming and I will not go before my name be known by everyone in the world!"<<std::endl;
   return 1;
 }
