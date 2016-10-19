ec90e287c6005fdee6b1999fdc3b071cdd386a14
nextdreamblue <zxw520blue1@163.com>
2016-10-19 11:14:32 +0800

    helloworld.cpp
    ...

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+/* 
+My first project on Github
+2016/10/19
+by nextdreamblue
+*/
+
+#include <iostream>
+
+int main()
+{
+  std::cout<<"hello world! I am coming and I will not go before my name be known by everyone in the world"<<std::endl;
+  return 1;
+}
